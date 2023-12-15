a6215d735861fa55725931d2ad68b0ab280fd6a8
Yana Stoyanidu <yana955@mail.ru>
2023-12-15 23:11:48 +0300

    change project name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/dist/',
+	base: '/polis812/',
 	plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
