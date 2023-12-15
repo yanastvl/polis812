1bdd85af006c23134bd821ce2e1b60ca79535787
Yana Stoyanidu <yana955@mail.ru>
2023-12-15 22:16:17 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
+	base: 'https://yanastvl.github.io/polis812/',
+	plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
 	],
